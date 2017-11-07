563360bd0910a7f46de13c527842d96751608086
OussamaSghaier <oussamasghaier1995@gmail.com>
2017-11-07 22:55:03 +0100

    output unifiation functions : mgu -> displayFunction

--- TP2/test_unifier.py
+++ TP2/test_unifier.py
@@ -5,7 +5,24 @@ from unification import *
 
 class TestUnifier(TestCase):
 
+
+    def displayFunction(self,ch):
+        s=str(ch).split(',')
+        #print(s)
+        i=0
+        for c in s:
+            if '[' in c:
+                print(c.split('[')[1].replace('\'',''),'(',end='')
+                i=i+1
+            elif ']' in c:
+                print(c.split(']')[0].replace('\'',''), end='')
+                for i in range(0,i):
+                    print(')',end='')
+            else:
+                print(c.replace('\'',''),',',end='')
+
     def mgu(self,exp1,exp2):
+
         d=unifier(exp1,exp2)
 
         if d is None:
@@ -13,7 +30,15 @@ class TestUnifier(TestCase):
             return
 
         for key, value in d.items():
-            print('(', key, '/', value, ')', end='')
+
+            if ']' in str(value):
+                print('(',key,'/ ',end='')
+                self.displayFunction(str(value))
+                print(')',end='')
+
+            else:
+                print('(', key, '/', value, ')', end='')
+
 
         print('\n')
 
@@ -23,6 +48,7 @@ class TestUnifier(TestCase):
         ex2 = Expression('?x')
         #print(unifier(ex,ex2))
         self.mgu(ex,ex2)
+        #print(isinstance("['f', 'A', 'C', 'B']", list))
 
     def test_unifier1(self):
         ex = Expression("p(B,C,?x,?z,f(A,?z,B))")
@@ -52,5 +78,23 @@ class TestUnifier(TestCase):
     def test_unifier4(self):
         ex = Expression("?x")
         ex2 = Expression("g(?y)")
+        #print(unifier(ex,ex2))
+        self.mgu(ex,ex2)
+
+    def test_unifier5(self):
+        ex = Expression("g(f(k(?x)))")
+        ex2 = Expression("?N")
+        #print(unifier(ex,ex2))
+        self.mgu(ex,ex2)
+
+    def test_unifier6(self):
+        ex = Expression("?x")
+        ex2 = Expression("f(g(?y,a,f(b)))")
+        #print(unifier(ex,ex2))
+        self.mgu(ex,ex2)
+
+    def test_unifier6(self):
+        ex = Expression("f(?r,?d,?d)")
+        ex2 = Expression("f(g(?y,a,f(b)),c,?z)")
         #print(unifier(ex,ex2))
         self.mgu(ex,ex2)
